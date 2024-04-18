998e6e9da81a1fc71b44385470279d9001cdfe77
chell4 <arsenijalfeev@gmail.com>
2024-04-18 18:58:24 +0300

    Mistral-46.7b Postgre Stuff

--- back/flake.nix
+++ back/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "fantastic hackathon go backend";
-  
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
@@ -12,6 +12,8 @@
     let 
       pkgs = import nixpkgs { inherit system; };
 
+      pkgsLinux = import nixpkgs { system = "x86_64-linux"; };
+
       backMod = pkgs.buildGoModule {
         name = "back";
 
@@ -19,15 +21,46 @@
 
         vendorHash = null;
       };
+      
+      dbContainer = pkgd.dockerTools.buildImageWithSettings {
+        name = "db";
 
+        baseImage = pkgs.dockerTools.alpineImage {
+          tag = "latest";
+          extraConfig = ''
+            RUN apk add --no-cache postgresql;
+          '';
+        };
+
+        cmd = [];
+        portMappings = [{
+          hostPort = 5432;
+          containerPort = 5432;
+        }];
+
+        entryPoint = "postgres";
+        envVars = {
+          POSTGRES_USER = "youruser";
+          POSTGRES_PASSWORD = "YourStr0ngP4ssword";
+          POSTGRES_DB = "yourdatabase";
+          PGDATA = "/tmp/pgdata";
+        };
+
+        volumes = ["${pkgs.writeText "pg_hba.conf" ''
+          local all all trust
+        ''}": "/tmp/pgdata/pg_hba.conf"];
+      };
     in
     {
-      packages.default = backMod;
+      packages = {
+        default = backContainer;
+        db = dbContainer;
+      };
 
       devShells = {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            go            
+            go
           ];
         };
       };
