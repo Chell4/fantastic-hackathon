22be7d55b705d01e91b3a9f5090ccf97fc167f70
RONJA06 <81020692+RONJA06@users.noreply.github.com>
2024-04-19 22:04:15 +0300

    upload picture

--- back/handlers/media.go
+++ back/handlers/media.go
@@ -1,7 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+)
 
 func (s *HandlersServer) HandleMedia(w http.ResponseWriter, r *http.Request) {
-
+	u, _ := url.Parse(r.URL.Path)
+	picName := path.Base(u.Path)
+	pic, err := os.ReadFile("../assets/" + picName)
+	if err != nil {
+		log.Fatalln("Failed read picture.", err)
+	} else {
+		w.Write(pic)
+	}
 }
