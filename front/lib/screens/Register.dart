e3a29560561b57c0c0c3ad0628d8168c4cd22ba1
velikoss <velikoss@vk.com>
2024-04-19 10:49:41 +0300

    Many changes

--- front/lib/screens/Register.dart
+++ front/lib/screens/Register.dart
@@ -3,7 +3,10 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:gif/gif.dart';
+import 'dart:html' as html;
+import 'package:go_router/go_router.dart';
 import 'package:iosish_shaker/iosish_shaker.dart';
+import 'package:lottie/lottie.dart';
 import 'package:material_text_fields/material_text_fields.dart';
 import 'package:material_text_fields/theme/material_text_field_theme.dart';
 import 'package:material_text_fields/utils/form_validation.dart';
@@ -24,114 +27,131 @@ class _RegisterState extends State<Register> with TickerProviderStateMixin  {
 
   final ShakerController shakeController = ShakerController();
 
-  late final GifController _gifController;
+  late final AnimationController _gifController;
 
   @override
   void initState() {
-    _gifController = GifController(vsync: this);
+    _gifController = AnimationController(vsync: this);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          double maxWidth = max(500, MediaQuery
-              .of(context)
-              .size
-              .width * 0.3);
+    return Scaffold(
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            double maxWidth = max(500, MediaQuery
+                .of(context)
+                .size
+                .width * 0.3);
 
-          return Center(
-            child: Container(
-              width: min(maxWidth, constraints.maxWidth),
-              alignment: Alignment.center,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                child: Form(
-                  key: _key,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Shaker(controller: shakeController, child: Gif(
-                          controller: _gifController,
-                          image: const AssetImage("IdentificationCard.webp"),
-                          width: min(maxWidth, constraints.maxWidth) * 0.5,
-                          fps: 20,
-                          autostart: Autostart.once,
-                          placeholder: (BuildContext context){
-                            return const CircularProgressIndicator();
-                          },
-                        )),
-                        SizedBox(
-                          height: min(maxWidth, constraints.maxWidth) * 0.15,
-                        ),
-                        MaterialTextField(
-                          keyboardType: TextInputType.text,
-                          hint: "Email",
-                          labelText: "Email",
-                          theme: FilledOrOutlinedTextTheme(
-                            enabledColor: Colors.grey,
-                            focusedColor: Colors.grey.shade400,
-                            fillColor: Colors.transparent,
+            return Center(
+              child: Container(
+                width: min(maxWidth, constraints.maxWidth),
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child: Form(
+                    key: _key,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Shaker(controller: shakeController, child:
+                          Lottie.asset(
+                              "IdentificationCard.json",
+                              controller: _gifController,
+                              width: min(maxWidth, constraints.maxWidth) * 0.6,
+                              frameRate: const FrameRate(60),
+                              onLoaded: (composition) {
+                                // Configure the AnimationController with the duration of the
+                                // Lottie file and start the animation.
+                                _gifController
+                                  ..duration = composition.duration
+                                  ..forward();
+                              },
+                              filterQuality: FilterQuality.medium
+                          )
                           ),
-                          textInputAction: TextInputAction.next,
-                          prefixIcon: const Icon(Icons.alternate_email),
-                          validator: FormValidation.emailTextField,
-                        ),
-                        SizedBox(
-                          height: min(maxWidth, constraints.maxWidth) * 0.025,
-                        ),
-                        MaterialTextField(
-                          keyboardType: TextInputType.text,
-                          hint: "Login",
-                          labelText: "Login",
-                          theme: FilledOrOutlinedTextTheme(
-                            enabledColor: Colors.grey,
-                            focusedColor: Colors.grey.shade400,
-                            fillColor: Colors.transparent,
+                          SizedBox(
+                            height: min(maxWidth, constraints.maxWidth) * 0.1,
                           ),
-                          textInputAction: TextInputAction.next,
-                          prefixIcon: const Icon(Icons.person),
-                          validator: Validation.requiredLogin,
-                        ),
-                        SizedBox(
-                          height: min(maxWidth, constraints.maxWidth) * 0.025,
-                        ),
-                        MaterialTextField(
-                          keyboardType: TextInputType.visiblePassword,
-                          obscureText: true,
-                          hint: "Password",
-                          labelText: "Password",
-                          theme: FilledOrOutlinedTextTheme(
-                            enabledColor: Colors.grey,
-                            focusedColor: Colors.grey.shade400,
-                            fillColor: Colors.transparent,
+                          MaterialTextField(
+                            keyboardType: TextInputType.text,
+                            hint: "Email",
+                            labelText: "Email",
+                            theme: FilledOrOutlinedTextTheme(
+                              enabledColor: Colors.grey,
+                              focusedColor: Colors.grey.shade400,
+                              fillColor: Colors.transparent,
+                            ),
+                            textInputAction: TextInputAction.next,
+                            prefixIcon: const Icon(Icons.alternate_email),
+                            validator: FormValidation.emailTextField,
                           ),
-                          textInputAction: TextInputAction.next,
-                          prefixIcon: const Icon(Icons.lock),
-                          validator: FormValidation.requiredTextField,
-                        ),
-                        SizedBox(
-                          height: min(maxWidth, constraints.maxWidth) * 0.05,
-                        ),
-                        ElevatedButton(
-                          onPressed: onSubmitBtnPressed,
-                          child: Text('Register', textScaler: TextScaler.linear(1.2), style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),),
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
+                          SizedBox(
+                            height: min(maxWidth, constraints.maxWidth) * 0.025,
+                          ),
+                          MaterialTextField(
+                            keyboardType: TextInputType.text,
+                            hint: "Login",
+                            labelText: "Login",
+                            theme: FilledOrOutlinedTextTheme(
+                              enabledColor: Colors.grey,
+                              focusedColor: Colors.grey.shade400,
+                              fillColor: Colors.transparent,
+                            ),
+                            textInputAction: TextInputAction.next,
+                            prefixIcon: const Icon(Icons.person),
+                            validator: Validation.requiredLogin,
+                          ),
+                          SizedBox(
+                            height: min(maxWidth, constraints.maxWidth) * 0.025,
+                          ),
+                          MaterialTextField(
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: true,
+                            hint: "Password",
+                            labelText: "Password",
+                            theme: FilledOrOutlinedTextTheme(
+                              enabledColor: Colors.grey,
+                              focusedColor: Colors.grey.shade400,
+                              fillColor: Colors.transparent,
+                            ),
+                            textInputAction: TextInputAction.next,
+                            prefixIcon: const Icon(Icons.lock),
+                            validator: FormValidation.requiredTextField,
+                          ),
+                          SizedBox(
+                            height: min(maxWidth, constraints.maxWidth) * 0.05,
+                          ),
+                          ElevatedButton(
+                            onPressed: onSubmitBtnPressed,
+                            child: Text('Register', textScaler: TextScaler.linear(1.2), style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
                             ),
                           ),
-                        )
-                      ],
+                          SizedBox(
+                            height: min(maxWidth, constraints.maxWidth) * 0.025,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              context.go("/login");
+                            },
+                            child: const Text("Sign In"),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          );
-        }
+            );
+          }
+      ),
     );
   }
 
