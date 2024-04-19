c1fb719aa1de978d0793aec1cf7549de703da114
RONJA06 <81020692+RONJA06@users.noreply.github.com>
2024-04-19 16:34:03 +0300

    lol registe

--- back/handlers/register.go
+++ back/handlers/register.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
+	"time"
 )
 
 const (
@@ -14,13 +18,34 @@ const (
 const BcryptCost = 10
 
 type RegisterRequest struct {
-	Login     string `json:"login"`
-	Passwords string `json:"password"`
-	Email     string `json:"email"`
+	GoogleID string `json:"google_id"`
+
+	FirstName   string `json:"first_name"`
+	SecondName  string `json:"second_name"`
+	LastName    string `json:"last_name"`
+	PicturePath string `json:"picture_path"`
 }
 
 type RegisterResponse struct {
 }
 
 func (s *HandlersServer) HandleRegister(w http.ResponseWriter, r *http.Request) {
+	var resp RegisterRequest
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalln("Enable to read data: ", err)
+	} else {
+		err = json.Unmarshal(body, &resp)
+		if err != nil {
+			log.Fatalln("Enable to convert data to json: ", err)
+		}
+	}
+	var cnt int64
+	s.DB.Where("google_id = ?", resp.GoogleID).Count(&cnt)
+	if cnt == 0 {
+		log.Fatalln("User with this ID doesn`t exist.")
+	} else {
+		s.DB.Model(&User{}).Where("google_id = ?", resp.GoogleID).Updates(User{FirstName: resp.FirstName, SecondName: &resp.SecondName, LastName: resp.LastName, PictureUrl: &resp.PicturePath, UpdatedAt: time.Now()})
+	}
 }
