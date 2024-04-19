c626a0197beabba76cdaed7456a982f9271fa0d4
chell4 <arsenijalfeev@gmail.com>
2024-04-19 17:33:39 +0300

    download profile picture

--- back/handlers/handlers.go
+++ back/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/patrickmn/go-cache"
@@ -13,6 +15,7 @@ const (
 	ErrStatusDatabaseErr           = "Error while interacting with database"
 	ErrStatusUserExists            = "User with this login already exists"
 	ErrStatusRegisterUserNotExists = "User with this id doesn't exist"
+	ErrStatusInvalidPhotoURL       = "Sent photo url is invalid"
 )
 
 type HandlersServer struct {
@@ -24,11 +27,20 @@ type HandlersServer struct {
 type User struct {
 	ID string `gorm:"primaryKey,size:16"`
 
-	FirstName  *string
-	SecondName *string
-	LastName   *string
-	PictureUrl *string
+	FirstName   *string
+	SecondName  *string
+	LastName    *string
+	PicturePath *string
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func CheckServerError(w http.ResponseWriter, err error) bool {
+	if err == nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Println(err)
+		return true
+	}
+	return false
 }
--- back/handlers/login.go
+++ back/handlers/login.go
@@ -1,14 +1,16 @@
 package handlers
 
 import (
+	"crypto/sha1"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 )
 
 type LoginRequest struct {
-	googleID    string `json:"google_id"`
-	googleToken string `json:"google_token"`
+	googleID string `json:"google_id"`
+	photoURL string `json:"photo_url"`
 }
 
 type LoginResponse struct {
@@ -19,6 +21,7 @@ func (s *HandlersServer) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	bodyJSON, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, ErrStatusCannotReadBody, http.StatusBadRequest)
+		log.Println(err)
 		return
 	}
 
@@ -26,6 +29,7 @@ func (s *HandlersServer) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	err = json.Unmarshal(bodyJSON, &logReq)
 	if err != nil {
 		http.Error(w, ErrStatusInvalidJSON, http.StatusBadRequest)
+		log.Println(err)
 		return
 	}
 
@@ -33,11 +37,33 @@ func (s *HandlersServer) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	err = s.DB.Table("users").Where("id = $1", logReq.googleID).Count(&userCnt).Error
 	if err != nil {
 		http.Error(w, ErrStatusDatabaseErr, http.StatusInternalServerError)
+		log.Println(err)
 		return
 	}
 
-	if userCnt == 0 {
-		http.Error(w, ErrStatusRegisterUserNotExists, http.StatusNotFound)
+	if userCnt != 0 {
+		http.Error(w, ErrStatusUserExists, http.StatusConflict)
+		log.Println(err)
 		return
 	}
+
+	photoResp, err := http.Get(logReq.photoURL)
+	if err != nil {
+		http.Error(w, ErrStatusInvalidPhotoURL, http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+
+	photoData, err := io.ReadAll(photoResp.Body)
+	if CheckServerError(w, err) {
+		return
+	}
+
+	hash := sha1.New()
+	_, err = hash.Write(photoData)
+	if CheckServerError(w, err) {
+		return
+	}
+
+	//err = s.DB.Table("users").Create(&User{})
 }
