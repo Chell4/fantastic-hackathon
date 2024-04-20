6b1db9ad4b04f9768553ef19e9b3865c8dfab058
chell4 <arsenijalfeev@gmail.com>
2024-04-20 10:10:57 +0300

    fix register user

--- back/handlers/register.go
+++ back/handlers/register.go
@@ -3,52 +3,69 @@ package handlers
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterRequest struct {
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	Phone       string `json:"phone"`
-	FirstName   string `json:"first_name"`
-	SecondName  string `json:"second_name"`
-	LastName    string `json:"last_name"`
-	PicturePath string `json:"picture_path"`
-}
-
-type RegisterResponse struct {
+	Phone     string `json:"phone"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 func (s *HandlersServer) HandleRegister(w http.ResponseWriter, r *http.Request) {
-	var req RegisterRequest
-
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		return
-	} else {
-		err = json.Unmarshal(body, &req)
-		if err != nil {
-			log.Fatalln("Enable to convert data to json: ", err)
-			return
-		}
-	}
-	var cnt int64
-	s.DB.Where("phone = ?", resp.Phone).Count(&cnt)
-	if cnt == 0 {
-		s.DB.Table("users").Create(&User{
-			ID:           uuid.NewString(),
-			FirstName:    resp.FirstName,
-			SecondName:   &resp.SecondName,
-			LastName:     resp.LastName,
-			PasswordHash: resp.PasswordHash,
-			Phone:        resp.PhoneNumber,
-			PicturePath:  &resp.PicturePath,
+		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
+			"type":    "data",
+			"reason":  "body",
+			"explain": ErrExplainCannotReadBody,
 		})
-	} else {
-		log.Fatalln("User with current phone number already exist.")
+		return
+	}
+
+	var req RegisterRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
+			"type":    "data",
+			"reason":  "json",
+			"explain": ErrExplainInvalidJSON,
+		})
+		return
+	}
+
+	var cntUsers int64
+	err = s.DB.Where("phone = ?", req.Phone).Count(&cntUsers).Error
+	if CheckServerError(w, err) {
+		return
+	}
+
+	if cntUsers != 0 {
+		ErrorMap(w, http.StatusConflict, map[string]interface{}{
+			"type":    "register",
+			"reason":  "phone_exist",
+			"explain": ErrExplainPhoneExists,
+		})
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(req.Password)[:72], BcryptCost)
+	if CheckServerError(w, err) {
+		return
+	}
+
+	err = s.DB.Table("users").Create(&User{
+		ID:           uuid.NewString(),
+		FirstName:    req.FirstName,
+		LastName:     req.LastName,
+		PasswordHash: hash,
+		Phone:        req.Phone,
+	}).Error
+	if CheckServerError(w, err) {
 		return
 	}
 }
