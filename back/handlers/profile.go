23271c8c58e89666a3172409ebf04c552e34d909
chell4 <arsenijalfeev@gmail.com>
2024-04-20 18:20:15 +0300

    edit profile

--- back/handlers/profile.go
+++ back/handlers/profile.go
@@ -1,11 +1,21 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 )
 
-type ProfileResponse struct {
+type ProfileGetResponse struct {
+	FirstName   string `json:"first_name"`
+	SecondName  string `json:"second_name"`
+	LastName    string `json:"last_name"`
+	Phone       string `json:"phone"`
+	PicturePath string `json:"picture_path"`
+}
+
+type ProfilePostRequest struct {
 	FirstName   string `json:"first_name"`
 	SecondName  string `json:"second_name"`
 	LastName    string `json:"last_name"`
@@ -39,11 +49,61 @@ func (s *HandlersServer) HandleProfileGet(w http.ResponseWriter, r *http.Request
 		picturePath = *user.PicturePath
 	}
 
-	ErrorMap(w, http.StatusOK, ProfileResponse{
+	ErrorMap(w, http.StatusOK, ProfileGetResponse{
 		FirstName:   user.FirstName,
 		SecondName:  secondName,
 		LastName:    user.LastName,
 		Phone:       user.Phone,
 		PicturePath: picturePath,
 	})
+}
+
+func (s *HandlersServer) HandleProfilePost(w http.ResponseWriter, r *http.Request) {
+	user, valid := s.ValidateToken(w, r)
+	if !valid {
+		return
+	}
+
+	reqJSON, err := io.ReadAll(r.Body)
+	if err != nil {
+		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
+			"type":    "data",
+			"reason":  "body",
+			"explain": ErrExplainCannotReadBody,
+		})
+		return
+	}
+
+	var req ProfilePostRequest
+	err = json.Unmarshal(reqJSON, &req)
+	if err != nil {
+		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
+			"type":    "data",
+			"reason":  "json",
+			"explain": ErrExplainInvalidJSON,
+		})
+	}
+
+	if req.FirstName != "" {
+		user.FirstName = req.FirstName
+	}
+	if req.SecondName != "" {
+		user.SecondName = &req.SecondName
+	}
+	if req.LastName != "" {
+		user.LastName = req.LastName
+	}
+	if req.Phone != "" {
+		user.Phone = req.Phone
+	}
+	if req.PicturePath != "" {
+		user.PicturePath = &req.PicturePath
+	}
+
+	err = s.DB.Table("users").Save(user).Error
+	if CheckServerError(w, err) {
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
