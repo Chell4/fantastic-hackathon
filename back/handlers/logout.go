77045e17ee272c51bf36fbc71bc889f9ae62f2a2
chell4 <arsenijalfeev@gmail.com>
2024-04-20 12:12:19 +0300

    logout

--- back/handlers/logout.go
+++ back/handlers/logout.go
@@ -2,4 +2,6 @@ package handlers
 
 import "net/http"
 
-func (s *HandlersServer) HandleLogout(w http.ResponseWriter, r *http.Request)
+func (s *HandlersServer) HandleLogout(w http.ResponseWriter, r *http.Request) {
+	return
+}
