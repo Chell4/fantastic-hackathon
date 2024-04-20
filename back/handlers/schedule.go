0338042abb9a32fee6d832617f2acc50f23492d7
chell4 <arsenijalfeev@gmail.com>
2024-04-20 14:25:21 +0300

    schedule

--- back/handlers/schedule.go
+++ back/handlers/schedule.go
@@ -17,12 +17,15 @@ func (s *HandlersServer) HandleSchedule(w http.ResponseWriter, r *http.Request) 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{})
-	} else {
-		err = json.Unmarshal(body, &req)
-		if err != nil {
-			ErrorMap(w, http.StatusBadRequest, map[string]interface{}{})
-		}
+		return
 	}
+
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{})
+		return
+	}
+
 	if s.CronProcess != nil {
 		s.Cron.Remove(*s.CronProcess)
 		id, err := s.Cron.AddFunc(fmt.Sprintf("CRON_TZ=Europe/Moscow %v", req.Settings), func() {})
