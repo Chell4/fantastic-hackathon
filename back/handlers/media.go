3e8e036911ada6e781754bc2b17a13677e574eea
chell4 <arsenijalfeev@gmail.com>
2024-04-20 16:56:15 +0300

    replace media handlers

--- back/handlers/media.go
+++ back/handlers/media.go
@@ -24,6 +24,32 @@ func (s *HandlersServer) HandleMedia(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (s *HandlersServer) HandleMediaGet(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	path, has := vars["path"]
+	if !has {
+		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
+			"type":    "data",
+			"reason":  "path",
+			"explain": ErrExplainInvalidPhotoURL,
+		})
+		return
+	}
+
+	pic, err := os.ReadFile("media/" + path)
+	if err != nil {
+		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
+			"type":    "media",
+			"reason":  "path",
+			"explain": ErrExplainInvalidMedia,
+		})
+		return
+	}
+
+	w.Write(pic)
+}
+
 func (s *HandlersServer) HandleMediaPost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	path, has := vars["path"]
@@ -59,30 +85,4 @@ func (s *HandlersServer) HandleMediaPost(w http.ResponseWriter, r *http.Request)
 	if CheckServerError(w, err) {
 		return
 	}
-}
-
-func (s *HandlersServer) HandleMediaGet(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	path, has := vars["path"]
-	if !has {
-		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
-			"type":    "data",
-			"reason":  "path",
-			"explain": ErrExplainInvalidPhotoURL,
-		})
-		return
-	}
-
-	pic, err := os.ReadFile("media/" + path)
-	if err != nil {
-		ErrorMap(w, http.StatusBadRequest, map[string]interface{}{
-			"type":    "media",
-			"reason":  "path",
-			"explain": ErrExplainInvalidMedia,
-		})
-		return
-	}
-
-	w.Write(pic)
 }
